a61c77a9833a89956be78e84d653efc1fe6b82dc
knyongki <yyongkisetiawan@gmail.com>
2023-09-17 14:25:24 +0700

    authentication implemented

--- src/api/notes/routes.js
+++ src/api/notes/routes.js
@@ -2,20 +2,32 @@ const routes = (handler) => [{
   method: 'POST',
   path: '/notes',
   handler: handler.postNotesHandler,
+  options: {
+    auth: 'notesapp_jwt',
+  },
 },
 {
   method: 'GET',
   path: '/notes',
   handler: handler.getNotesHandler,
+  options: {
+    auth: 'notesapp_jwt',
+  },
 }, {
   method: 'GET',
   path: '/notes/{id}',
   handler: handler.getNoteByIdHandler,
+  options: {
+    auth: 'notesapp_jwt',
+  },
 },
 {
   method: 'PUT',
   path: '/notes/{id}',
   handler: handler.putNoteByIdHandler,
+  options: {
+    auth: 'notesapp_jwt',
+  },
 },
 {
   method: 'DELETE',
